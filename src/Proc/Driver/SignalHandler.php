178572ef9b5ac647c51ff732b035b24eff781269
Philip Whitt <philip.whitt@sbcglobal.net>
2020-10-07 09:42:47 -0500

    Adding more sig handlers. Adding SIGWINCH handler for apache debian docker

--- src/Proc/Driver/SignalHandler.php
+++ src/Proc/Driver/SignalHandler.php
@@ -11,13 +11,29 @@ class SignalHandler
     {
         if (function_exists('pcntl_async_signals')) {
             pcntl_async_signals(true);
-            pcntl_signal(SIGHUP,  [$scope, $callback]);
-            pcntl_signal(SIGTERM, [$scope, $callback]);
-            pcntl_signal(SIGQUIT, [$scope, $callback]); // Ctrl-\
-            pcntl_signal(SIGINT,  [$scope, $callback]); // Ctrl-C
-            pcntl_signal(SIGTSTP, [$scope, $callback]);
+
+            pcntl_signal(SIGTERM,  [$scope, $callback]);
+            pcntl_signal(SIGHUP,   [$scope, $callback]);
+            pcntl_signal(SIGTSTP,  [$scope, $callback]);
+            pcntl_signal(SIGUSR1,  [$scope, $callback]);
+            pcntl_signal(SIGILL,   [$scope, $callback]);
+
+            // Ctrl-\
+            pcntl_signal(SIGQUIT,  [$scope, $callback]);
+            
+            // Ctrl-\
+            pcntl_signal(SIGABRT,  [$scope, $callback]);
+            
+            // Ctrl-C
+            pcntl_signal(SIGINT,   [$scope, $callback]);
+
+            // for apache on debian
+            // see: https://github.com/docker-library/php/blob/master/7.4/buster/apache/Dockerfile#L275
+            pcntl_signal(SIGWINCH, [$scope, $callback]);
+
         } else {
-            Log::warning('pcntl methods not found');
+            Log::withName('SignalHandler')
+                ->warning('pcntl methods not found. sig handler will not work.');
         }
     }
 
